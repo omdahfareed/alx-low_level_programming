40ab63f8a576b34b6d4a228a3d3263ea31f2c983
omdahfareed <tr07477@gmail.com>
2023-08-10 22:47:02 +0200

    x

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 1 (error)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1, quo, 59);
+	return (1);
+}
