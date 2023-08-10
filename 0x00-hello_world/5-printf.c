2f481a1b6dcebfd53e2dcc3bb8f1cd40fdf89bac
omdahfareed <tr07477@gmail.com>
2023-08-10 21:10:24 +0200

    x

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
  *
  * Description: A c program that prints with
  printf function                             *
- * Return: Always 0 (success)               */                                          int main(void)                              {                                                   printf("with proper grammar, but the outcome is a piece of art,");
+ * Return: Always 0 (success)               */                                          int main(void)                              {                                                   printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
