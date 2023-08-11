f8166ec7cbc22cf278b9e63fc34f317ff732bbf8
omdahfareed <tr07477@gmail.com>
2023-08-11 22:21:54 +0200

    x

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
