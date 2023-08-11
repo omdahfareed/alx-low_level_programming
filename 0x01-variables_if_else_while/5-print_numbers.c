af485a0e132883a3bf11d05a26750754eaed5dce
omdahfareed <tr07477@gmail.com>
2023-08-11 22:05:54 +0200

    x

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9 )
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
