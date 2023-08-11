93b2d3e4abcc87060e0b0253f11a62afef087b3c
omdahfareed <tr07477@gmail.com>
2023-08-11 23:20:59 +0200

    x

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(' ');
+		        putchar(',');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
