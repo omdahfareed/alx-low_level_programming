8a0126403c5060d3b33862876e16c066ae64c3b2
omdahfareed <tr07477@gmail.com>
2023-08-12 01:16:47 +0200

    x

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digt = 0, digt1;
+	while (digt <= 99)
+	{
+
+		digt1 = digt;
+		while (digt1 <= 99)
+		{
+			if (digt1 != digt)
+			{
+				putchar((digt / 10) + '0');
+				putchar((digt % 10) + '0');
+				putchar(' ');
+				putchar((digt1 / 10) + '0');
+				putchar((digt1 % 10) + '0');
+				if (digt + digt1 != 197)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digt1++;
+		}
+		digt++;
+	}
+	putchar('\n');
+
+	return (0);
+}
