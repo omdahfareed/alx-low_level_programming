53751aa72af760c14a15b4265f107a0eca70a14b
omdahfareed <tr07477@gmail.com>
2023-08-15 22:12:56 +0200

    x

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch <= 0; ch++)
+	{
+		_putchar(a[ch]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write acharacter c to stdout
+ * c : character to print
+ *
+ * Return: 1 (success) , -1 (error)
+*/
+int _putchar(char c);
+{
+	return (write(1, &c, 1));
+}
