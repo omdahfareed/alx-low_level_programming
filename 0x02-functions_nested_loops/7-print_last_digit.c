aa48c9e13c040edd31c534ea946fad42171b120a
omdahfareed <tr07477@gmail.com>
2023-08-16 00:46:11 +0200

    x

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,17 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
-		n = -1 * (n % 10);
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + '0');
 	}
-	_putchar((n % 10) + 48);
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+	}
 	return (n);
 }
