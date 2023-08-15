7d64c2109ac54e02c92f45b2c7757d0155006dd5
omdahfareed <tr07477@gmail.com>
2023-08-15 23:26:33 +0200

    x

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,6 +10,8 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
 		return (1);
-	return (0)
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
