8863e45b8ea56d9092a286978466af3785113771
omdahfareed <tr07477@gmail.com>
2023-08-15 22:35:16 +0200

    x

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet using _putchar function
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
