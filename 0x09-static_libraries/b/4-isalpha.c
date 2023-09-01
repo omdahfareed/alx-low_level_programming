1610092a95e16986eceda1ef0e3e23ce535c0876
omdahfareed <tr07477@gmail.com>
2023-09-01 23:07:19 +0200

    x

---
+++ 0x09-static_libraries/b/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ *
+ * @c: checks for inputs
+ *
+ * Return: 1  if c is a letter, lowercase or uppercase , 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
